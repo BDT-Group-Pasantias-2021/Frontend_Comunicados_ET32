22a7546f0c49eaa5666f4d97213dd5f05124f629
Tisera-Lucas <tiseralucas2002@gmail.com>
2021-09-09 18:43:32 -0300

    Feat - Vista login con Api para iniciar sesion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 // Components
 
+import Login from "./components/views/Login"
+import Home from "./components/views/Inicio"
+import Navbar from "./components/common/Navbar/Navbar"
+
 // Styles
+
+import './css/global.css'
 
 // Navigation
 
 export default function App() {
-	return <div></div>;
+	return <Router basename="/Frontend_Comunicados_ET32">
+				{/* <Navbar /> */}
+				<Switch>
+					<Route path="/home" component={Home} />
+					<Route path="/" exact component={Login} />
+				</Switch>
+			</Router>;
 }
